504a01e45b24325813baf543c619f3a5aa910ef8
Rob Thomson <rob@marotori.com>
2026-06-30 16:01:22 +0100

    Fix flight mode sensor

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -642,6 +642,12 @@ void processRxModes(timeUs_t currentTimeUs)
 
     updateActivatedModes();
 
+    if (IS_RC_MODE_ACTIVE(BOXPASSTHROUGH)) {
+        ENABLE_FLIGHT_MODE(PASSTHROUGH_MODE);
+    } else {
+        DISABLE_FLIGHT_MODE(PASSTHROUGH_MODE);
+    }
+
     if (!cliMode &&
 #ifdef USE_CMS
         !cmsInMenu &&
